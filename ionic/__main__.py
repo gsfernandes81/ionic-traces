1f0c05613a0c882bba48039f531f3b0d7512c821
GS <gsfernandes81@gmail.com>
2022-12-15 21:20:47 +0000

    Fixed BRUCE reactions when from unknown guild

--- ionic/__main__.py
+++ ionic/__main__.py
@@ -170,6 +170,13 @@ class Bot(lb.BotApp):
                     # since emoji_id is None for these. emoji_name will have the
                     # emoji in this case
                     reaction = emoji_name
+                except h.NotFoundError:
+                    for reaction in [r.emoji for r in msg.reactions]:
+                        if (
+                            isinstance(reaction, h.CustomEmoji)
+                            and event.emoji_id == reaction.id
+                        ):
+                            break
                 await msg.add_reaction(reaction)
 
         return self.listen()(reaction_handler)
